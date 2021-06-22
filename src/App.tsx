9bc17dd676551c0b724fa9700d46eb0631cdf8b5
Joao Diego Florencio <jdflorencio@outlook.com>
2021-06-22 01:41:21 -0300

    entendo componentes e props(atributos)

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
-
+import { Button } from './components/Button'
 function App() {
   return (
-  <h1>Hello World</h1>
+    <div>
+      <Button text="Botão 1"/>
+      <Button>Clique Aqui 2</Button>
+    </div>
   );
 }
 
