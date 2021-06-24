6e47849c507e7e4a475a005fba17460dd67492c4
Joao Diego Florencio <jdflorencio@outlook.com>
2021-06-24 05:39:13 -0300

    estilizando a pagina newroom

---
+++ src/pages/NewRoom.tsx
@@ -0,0 +1,35 @@
+import ilustrationImg from '../assets/images/illustration.svg'
+import logoImg from '../assets/images/logo.svg'
+import '../styles/auth.scss'
+import { Button } from '../components/Button'
+
+export function NewRoom() {
+    return (
+        <div id="page-auth">
+            <aside>
+                <img src={ilustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
+                <strong>Crie salas de Q&amp;A ao-vivo</strong>
+                <p>
+                    Tire suas dúvidas em tempo-real
+                </p>
+            </aside>
+            <main>
+                <div className="main-content">
+                    <img src={logoImg} alt="letMeask" />
+                    <h2>Criar uma nova Sala</h2>
+                    <form >
+                        <input
+                            type="text"
+                            placeholder="Digite o nome da sala" />
+                        <Button type="submit">
+                            Criar sala
+                        </Button>
+                    </form>
+                    <p>
+                        Quer entrar em uma sala já existente? <a href="#">clique aqui</a>
+                    </p>
+                </div>
+            </main>
+        </div>
+    )
+}
