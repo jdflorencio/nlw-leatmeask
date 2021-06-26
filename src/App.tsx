0a618c5a79493cb97169eedff77ebdf0dd1c4d0a
Joao Diego Florencio <jdflorencio@outlook.com>
2021-06-25 21:58:16 -0300

    criando a tela login

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
+import { createContext, useState } from 'react'
 import { BrowserRouter, Route } from 'react-router-dom'
 import { Home } from "./pages/Home"
 import { NewRoom } from './pages/NewRoom'
 
+export const AuthContext = createContext({} as any)
+
 function App() {
+  const [ user, setUser] = useState()
   return (
     <div>
       <BrowserRouter>
-        <Route path="/" exact={true} component={Home} />
-        <Route path="/rooms/new" component={NewRoom} />
+        <AuthContext.Provider value={{user, setUser}}>
+          <Route path="/" exact={true} component={Home} />
+          <Route path="/rooms/new" component={NewRoom} />
+        </AuthContext.Provider>
       </BrowserRouter>
-      
+
     </div>
   );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,26 @@
+import { useContext} from 'react'
+import { useHistory } from 'react-router-dom'
 import ilustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
 import googleImg from '../assets/images/google-icon.svg'
 import '../styles/auth.scss'
 import { Button } from '../components/Button'
+import { auth,firebase } from '../services/firebase'
+
 
 export function Home() {
+    const history = useHistory();
+    
+
+    function handleCreateRoom() {
+        const provider = new firebase.auth.GoogleAuthProvider()
+        auth.signInWithPopup(provider).then(result => {
+            console.log(result)
+            history.push('/rooms/new')
+        })
+
+    }
+
     return (
         <div id="page-auth">
             <aside>
@@ -15,9 +31,10 @@ export function Home() {
                 </p>
             </aside>
             <main>
+                
                 <div className="main-content">
                     <img src={logoImg} alt="letMeask" />
-                    <button className="create-room">
+                    <button onClick={handleCreateRoom} className="create-room">
                         <img src={googleImg} alt="" />
                         Crie sua sala com o Google
                     </button>
